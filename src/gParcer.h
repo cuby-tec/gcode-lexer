8a31452fb274271198af8d8b544d59b39c5fd355
Valery <walery@cuby.net>
2018-07-18 17:10:41 +0300

    #19 gParcer.h changed.

--- src/gParcer.h
+++ src/gParcer.h
@@ -30,29 +30,7 @@ extern void b_gcomment (size_t curline, char * param, size_t len);
 extern void b_g_command (size_t curline, char * param, size_t len);
 extern void b_o_command (curline, param, len); // O command
 extern void b_x_coordinate(size_t curline, char * param, size_t len);
-extern void b_y_coordinate(size_t curline, char * param, size_t len);
-extern void b_z_coordinate(size_t curline, char * param, size_t len);
-extern void b_a_parameter(size_t curline, char * param, size_t len);
-extern void b_b_parameter(size_t curline, char * param, size_t len);
-extern void b_c_parameter(size_t curline, char * param, size_t len);
-extern void b_d_parameter(size_t curline, char * param, size_t len);
-extern void b_e_parameter(size_t curline, char * param, size_t len);
-extern void b_f_parameter(size_t curline, char * param, size_t len);
-extern void b_i_parameter(size_t curline, char * param, size_t len);
-extern void b_j_parameter(size_t curline, char * param, size_t len);
-extern void b_k_parameter(size_t curline, char * param, size_t len);
-extern void b_l_parameter(size_t curline, char * param, size_t len);
-extern void b_m_parameter(size_t curline, char * param, size_t len);
-extern void b_n_parameter(size_t curline, char * param, size_t len);
-extern void b_p_parameter(size_t curline, char * param, size_t len);
-extern void b_r_parameter(size_t curline, char * param, size_t len);
-extern void b_s_parameter(size_t curline, char * param, size_t len);
-extern void b_t_parameter(size_t curline, char * param, size_t len);
-extern void b_u_parameter(size_t curline, char * param, size_t len);
-extern void b_v_parameter(size_t curline, char * param, size_t len);
-extern void b_w_parameter(size_t curline, char * param, size_t len);
-extern void b_star_parameter(size_t curline, char * param, size_t len);
-extern void b_punct(curline, param, len);
+//extern void b_punct(curline, param, len);
 #endif
 
 
